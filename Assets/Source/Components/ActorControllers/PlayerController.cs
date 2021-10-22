bd4c79b08f896315b4d55232277f936290d10565
Jordan Bleu <jordanbleu1@gmail.com>
2021-10-22 12:27:05 -0500

    add platforming poc scene

---
+++ Assets/Source/Components/ActorControllers/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Source.Components.Input;
+using UnityEngine;
+
+namespace Assets.Source.Components.ActorControllers
+{
+    /// <summary>
+    /// The player controller takes controls from user input and applies them to the character
+    /// </summary>
+    public class PlayerController : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Source/Components/Input/InputEventReciever.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Source.Components.Input
+{
+
+    /// <summary>
+    /// This component adds a level of abstraction between the code that handles user inputs and the code that responds to said inputs.  
+    /// This should be used when responding to user controls as it is input system agnostic.
+    /// </summary>
+    public class InputEventReciever : MonoBehaviour
+    {
+
+
+
+        public void OnJump(InputValue inputValue) {
+            UnityEngine.Debug.Log(inputValue);
+        }
+
+        ///// <summary>
+        ///// Value between -1 and 1 that represents the current requested horizontal movement speed.
+        ///// This will change depending on how much the analog stick is held
+        ///// </summary>
+        //public float RequestedHorizontalMovementSpeed { get; set; } = 0f;
+
+        //public void OnRequestMove(InputAction.CallbackContext context) {
+        //    UnityEngine.Debug.Log("hello");
+        //    RequestedHorizontalMovementSpeed = context.ReadValue<Vector2>().x;
+        //}
+
+    }
+}
