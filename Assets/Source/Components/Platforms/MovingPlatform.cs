5d4988d284385972df44fbc89b536080d0097f0a
Jordan Bleu <jordanbleu1@gmail.com>
2021-11-06 11:28:38 -0500

    Add Moving Platforms

---
+++ Assets/Source/Components/Platforms/MovingPlatform.cs
@@ -0,0 +1,240 @@
+﻿using Assets.Editor.Attributes;
+using Assets.Source.Components.Timer;
+using Assets.Source.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Source.Components.Platforms
+{
+
+    /// <summary>
+    /// Moving Platforms simply loop among multiple platforming instructions
+    /// </summary>
+    [RequireComponent(typeof(Collider2D))]
+    public class MovingPlatform : MonoBehaviour
+    {
+        private const float POSITION_TOLERANCE = 0.1f;
+
+        [SerializeField]
+        private MoveMode MovementBehavior;
+
+        [SerializeField]
+        private List<PlatformInstruction> instructions;
+
+        [SerializeField]
+        [ReadOnly]
+        private PlatformInstruction current;
+
+        [SerializeField]
+        private Rigidbody2D rigidBody;
+
+        private GameTimer timer;
+
+
+        private int index;
+        private int indexDirection = 1;
+        private bool wasAtDestination = false;
+
+
+
+        private void Start()
+        {
+            timer = gameObject.AddComponent<GameTimer>();
+            
+            timer.onTimerReachZero = new UnityEngine.Events.UnityEvent();
+            timer.onTimerReachZero.AddListener(GetNextInstruction);
+            timer.Label = "~ Auto-Generated Moving Platform Timer ~";
+            index = 0;
+        }
+
+        private void Update()
+        {
+            // Make sure index isn't out of bounds (can happen if manipulating values in inspector)
+            index = Mathf.Min(index, instructions.Count() - 1);
+
+            var currentInstruction = instructions[index];
+            current = currentInstruction; // for the unity inspector
+
+            if (!timer.IsActive)
+            {
+                if (IsNearDestination(currentInstruction))
+                {
+                    if (!wasAtDestination)
+                    {
+                        currentInstruction.onArrive?.Invoke();
+                    }
+                    wasAtDestination = true;
+                    timer.StartTime = currentInstruction.WaitTime;
+                    timer.StartTimer();
+                }
+                else
+                {
+                    wasAtDestination = false;
+                }
+
+            }
+
+            UpdateVelocity(currentInstruction);
+            
+        }
+
+        private void GetNextInstruction()
+        {
+            if (MovementBehavior != MoveMode.Manual)
+            {
+                if (MovementBehavior == MoveMode.Random)
+                {
+                    index = UnityEngine.Random.Range(0, (instructions.Count()));
+                }
+                index += indexDirection;
+
+                if (index > (instructions.Count() - 1) || index < 0)
+                {
+                    if (MovementBehavior == MoveMode.Cycle)
+                    {
+                        index = 0;
+                    }
+                    else
+                    {
+                        indexDirection = -indexDirection;
+                        index += indexDirection;
+                    }
+                }
+            }
+        }
+
+        public void CycleNext()
+        {
+
+            index += 1;
+
+            if (index > (instructions.Count() - 1))
+            {
+                index = 0;
+            }
+
+        }
+
+        public void CyclePrev()
+        {
+            index -= 1;
+
+            if (index < 0)
+            {
+                index = instructions.Count() - 1;
+            }
+        }
+
+        public void CycleFirst()
+        {
+            index = 0;
+        }
+
+        public void CycleLast()
+        {
+            index = instructions.Count() - 1;
+        }
+
+        private bool IsNearDestination(PlatformInstruction currentInstruction) =>
+            (transform.position.x.IsWithin(POSITION_TOLERANCE, currentInstruction.Position.x)) &&
+            (transform.position.y.IsWithin(POSITION_TOLERANCE, currentInstruction.Position.y));
+
+
+        private void UpdateVelocity(PlatformInstruction currentInstruction)
+        {
+            float xv = 0f;
+            float yv = 0f;
+            float xp = rigidBody.position.x;
+            float yp = rigidBody.position.y;
+
+
+            // Move x towards instruction's x position
+            if (!transform.position.x.IsWithin(POSITION_TOLERANCE, currentInstruction.Position.x))
+            {
+                if (transform.position.x < currentInstruction.Position.x)
+                {
+                    xv = currentInstruction.MovementSpeed;
+                }
+                else if (transform.position.x > currentInstruction.Position.x)
+                {
+                    xv = -currentInstruction.MovementSpeed;
+                }
+            }
+            else
+            {
+                xp = currentInstruction.Position.x;
+            }
+
+            // Move y towards y position
+            if (!transform.position.y.IsWithin(POSITION_TOLERANCE, currentInstruction.Position.y))
+            {
+                if (transform.position.y < currentInstruction.Position.y)
+                {
+                    yv = currentInstruction.MovementSpeed;
+                }
+                else if (transform.position.y > currentInstruction.Position.y)
+                {
+                    yv = -currentInstruction.MovementSpeed;
+                }
+            }
+            else
+            {
+                yp = currentInstruction.Position.y;
+            }
+
+            rigidBody.position = new Vector2(xp, yp);
+            rigidBody.velocity = new Vector2(xv, yv);
+        }
+
+        [Serializable]
+        public struct PlatformInstruction
+        {
+            [Tooltip("The world space position to move the platform to")]
+            public Vector2 Position;
+
+            [Tooltip("The time to wait (in milliseconds) after the platform reaches its destination")]
+            public float WaitTime;
+
+            [Tooltip("The speed at which the platform moves to that position")]
+            public float MovementSpeed;
+
+            [Tooltip("The color to show for the gizmo.  Used for debugging / visualizing paths.")]
+            public Color GizmoColor;
+
+            [Tooltip("Event thrown when the platform arrives at this location")]
+            public UnityEvent onArrive;
+
+        }
+        public enum MoveMode
+        {
+            [Tooltip("Platform moves positions in order.  After it reaches the last position, it loops back to the first.")]
+            Cycle,
+            [Tooltip("Platform moves forward through all positions in order.  Once it reaches the last position, it cycles backwards back through the positions")]
+            Alternate,
+            [Tooltip("Platform chooses a position randomly each time")]
+            Random,
+            [Tooltip("Platform will only move when the 'CycleNext' method is called externally.")]
+            Manual
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (instructions.Any())
+            {
+
+                var collider = GetComponent<Collider2D>();
+
+                foreach (var inst in instructions)
+                {
+                    Gizmos.color = inst.GizmoColor;
+                    Gizmos.DrawWireCube(inst.Position, collider.bounds.size);
+                }
+            }
+        }
+
+    }
+}
+
---
+++ Assets/Source/Components/Timer/GameTimer.cs
@@ -0,0 +1,104 @@
+﻿using Assets.Editor.Attributes;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Source.Components.Timer
+{
+    /// <summary>
+    /// The newer evolution of <seealso cref="IntervalTimerComponent"/> that is a lot less reliant on custom code.
+    /// </summary>
+    public class GameTimer : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Friendly name for debugging only, no real functionality.")]
+        private string label = "{New Game Timer}";
+        /// <summary>
+        /// Friendly name for debugging only, no real functionality.
+        /// </summary>
+        public string Label { get => label; set => label = value; }
+
+        [SerializeField]
+        [Tooltip("The starting time on the timer.")]
+        private float startTime = 500f;
+
+        [SerializeField]
+        [Tooltip("The current time")]
+        private float time;
+
+        public float StartTime { get => startTime; set => startTime = value; }
+
+        [SerializeField]
+        [Tooltip("Event invoked when the timer reaches zero.")]
+        public UnityEvent onTimerReachZero = new UnityEvent();
+
+        [SerializeField]
+        [Tooltip("If true, the timer will begin counting down automatically when the object is awakened.  Otherwise, the StartTimer() method must be called.")]
+        private bool startOnAwake = false;
+
+        [SerializeField]
+        [ReadOnly]
+        private bool isActive = false;
+        public bool IsActive { get => isActive; }
+
+        [SerializeField]
+        [Tooltip("Set to true if you want to start the timer at the 'time' you specified.  Otherwise it will be set to max time on awake.")]
+        private bool useCustomStartTime = false;
+
+        private void Start()
+        {
+            if (!useCustomStartTime)
+            {
+                time = startTime;
+            }
+
+            if (startOnAwake)
+            {
+                StartTimer();
+            }
+        }
+
+        private void Update()
+        {
+            if (isActive)
+            {
+                time -= (Time.deltaTime * 1000);
+
+                if (time <= 0)
+                {
+                    isActive = false;
+                    onTimerReachZero?.Invoke();
+                }
+            }
+            
+        }
+
+
+        /// <summary>
+        /// Reset the timer to its start time
+        /// </summary>
+        public void ResetTimer()
+        {
+            time = startTime;
+        }
+
+        /// <summary>
+        /// Start counting down the timer from the current time
+        /// </summary>
+        public void StartTimer()
+        {
+            isActive = true;
+        }
+
+        /// <summary>
+        /// Randomizes the current time to between zero and the start time
+        /// </summary>
+        public void RandomizeCurrentTime()
+        {
+            time = UnityEngine.Random.Range(0, startTime);
+        }
+
+
+
+
+    }
+}
