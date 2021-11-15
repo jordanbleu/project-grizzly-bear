157803246986815b85a468dc8fda6f0fdf1ba48b
Jordan Bleu <jordanbleu1@gmail.com>
2021-11-15 11:40:09 -0600

    added post fx and tutorial messages

---
+++ Assets/Source/Components/Animators/TutorialAnimator.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Assets.Source.Components.Animators
+{
+    public class TutorialAnimator : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Animator animator;
+
+
+        [SerializeField]
+        private TextMeshProUGUI textMesh;
+
+        //[SerializeField]
+        //private TextMeshProUGUI fieldName;
+
+        public void Show(string text) {
+            textMesh.SetText(text);
+            animator.SetTrigger("show");        
+        }
+
+        public void Hide() {
+            animator.SetTrigger("hide");
+        }
+
+
+
+
+    }
+}
---
+++ Assets/Source/Components/Triggers/HorizontalFrameTransition.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace Assets.Source.Components.Triggers
+{
+    /// <summary>
+    /// Handles seamless transition from one frame to another.
+    /// While the player is inside the attached trigger, both frames are enabled.  Upon exiting the left or right 
+    /// frame will be enabled depending on the position of the player.  
+    /// 
+    /// For this reason, the frames should overlap a tiny bit.
+    /// </summary>
+    [RequireComponent(typeof(Collider2D))]
+    public class HorizontalFrameTransition : MonoBehaviour
+    {
+
+        [SerializeField]
+        [Tooltip("Frame to enable if the player exist to the left of the transition")]
+        private GameObject leftFrame;
+
+
+        [SerializeField]
+        [Tooltip("Frame to enable if the player exist to the right of the transition")]
+        private GameObject rightFrame;
+
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                leftFrame.gameObject.SetActive(true);
+                rightFrame.gameObject.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player")) {
+
+                if (collision.gameObject.transform.position.x < transform.position.x)
+                {
+                    rightFrame.SetActive(false);
+                }
+                else {
+                    leftFrame.SetActive(false);
+                }
+
+            }            
+        }
+
+
+    }
+}
