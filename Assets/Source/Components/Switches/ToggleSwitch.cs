9de6da9d9329d633be2b1bb2471470220194169c
Jordan Bleu <jordanbleu1@gmail.com>
2022-04-12 06:26:09 -0500

    robot animation sequence

--- Assets/Source/Components/Switches/ToggleSwitch.cs
+++ Assets/Source/Components/Switches/ToggleSwitch.cs
@@ -5,16 +5,27 @@ namespace Assets.Source.Components.Switches
 {
     public class ToggleSwitch : MonoBehaviour
     {
-        
+
+        private bool wasToggled = false;
+
         [SerializeField]
         private UnityEvent onToggleOn = new UnityEvent();
 
         [SerializeField]
         private UnityEvent onToggleOff = new UnityEvent();
 
+        [SerializeField]
+        private UnityEvent onToggleFirstTime = new UnityEvent();
+
         public void Toggle(bool isOn) {
 
             if (isOn) {
+
+                if (!wasToggled) {
+                    onToggleFirstTime?.Invoke();
+                    wasToggled = true;
+                }
+
                 onToggleOn?.Invoke();
             } else { 
                 onToggleOff?.Invoke();            
