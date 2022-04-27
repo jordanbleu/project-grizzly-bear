47e52470309360c9c351cfc7dd27a52e7b687968
Jordan Bleu <jordanbleu1@gmail.com>
2022-04-26 20:34:07 -0500

    updates

---
+++ Assets/Source/Components/ActorControllers/Destructible.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Source.Components.ActorControllers
+{
+    public class Destructible : MonoBehaviour
+    {
+
+        [SerializeField]
+        private int health = 10;
+        
+        [SerializeField]
+        private UnityEvent onHealthDecreased = new UnityEvent();
+
+        public void IncreaseHealth(int amount) {
+            health += amount;
+        }
+
+        public void DecreaseHealth() {
+            health -= 10;
+            onHealthDecreased?.Invoke();
+        }    
+
+    }
+
+}
