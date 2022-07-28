0b39721942d8c9e97ca02ca67cea4e1d5e7d3165
Jordan Bleu <jbleu@jbleu-mac.local>
2022-07-28 16:50:32 -0500

    updates

---
+++ Assets/Source/Components/Animators/BeginningCutscene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Source.Components.Animators
+{
+	/// <summary>
+    /// A mostly useless class but its just how it's gotta be.
+    /// </summary>
+    public class BeginningCutscene : MonoBehaviour
+	{
+
+		[SerializeField]
+		private GameObject menuObject;
+
+		public void ShowMenu() =>
+			menuObject.SetActive(true);
+		
+
+	}
+
+}
---
+++ Assets/Source/Components/UI/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Source.Components.UI
+{
+    public class MainMenuController : MonoBehaviour
+    {
+
+        private Animator animator;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            
+        }
+
+        // called from Player Input component
+        public void OnAnyKey() {
+            animator.SetTrigger("dismiss");
+        }
+
+
+    }
+
+}
